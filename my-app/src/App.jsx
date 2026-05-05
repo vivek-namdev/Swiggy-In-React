5874b58949aeb0e7a474c3787b295c6401d816d2
Vivek Namdev <viveknamdev257@gmail.com>
2026-05-05 18:01:39 +0530

    added app banner

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import Dineoption from "./components/Dineoption"
 import FoodOption from "./components/FoodOption"
 import GroceryOption from "./components/GroceryOption"
 import Header from "./components/Header"
+import Banner from "./components/Banner"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <FoodOption></FoodOption>
       <GroceryOption></GroceryOption>
       <Dineoption></Dineoption>
+      <Banner></Banner>
     </>
   )
 }
---
+++ my-app/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="mt-10 mx-30 mb-10">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/seo/App_download_banner.png"
+        alt="Swiggy Banner"
+        className="w-full h-auto rounded-lg"
+      />
+    </div>
+  );
+};
+
+export default Banner;
